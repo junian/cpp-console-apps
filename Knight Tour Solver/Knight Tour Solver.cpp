f2b2d741c64ecd298adfaaf9a3955742fdf2fe28
junian <junian.triajianto@gmail.com>
2013-08-29 13:27:14 +0700

    added Knight's Tour solver

---
+++ Knight Tour Solver/Knight Tour Solver.cpp
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+//mencari jumlah kemungkinan suatu titik untuk menuju ke titik lain sesuai
+//langkah kuda
+int get_chance(char **board, int x, int y){
+    int chance = 0;
+    
+    if(x+2<8  && y+1<8  && board[x+2][y+1]==' ') chance++;
+    if(x+2<8  && y-1>=0 && board[x+2][y-1]==' ') chance++;
+    if(x-2>=0 && y+1<8  && board[x-2][y+1]==' ') chance++;
+    if(x-2>=0 && y-1>=0 && board[x-2][y-1]==' ') chance++;
+    
+    if(y+2<8  && x+1<8  && board[x+1][y+2]==' ') chance++;
+    if(y+2<8  && x-1>=0 && board[x-1][y+2]==' ') chance++;
+    if(y-2>=0 && x+1<8  && board[x+1][y-2]==' ') chance++;
+    if(y-2>=0 && x-1>=0 && board[x-1][y-2]==' ') chance++;
+    
+    return chance;
+}
+
+//keputusan kuda untuk bergerak berikutnya
+//dengan cara mencari kemungkinan gerak suatu titik berikutnya
+//yang terendah
+void next_step(char **board, int *posX, int*posY){
+    int chance = 8;
+    int x = *posX;
+    int y = *posY;
+    
+    if(x+2<8  && y+1<8  && board[x+2][y+1]==' '){
+        if(get_chance(board, x+2, y+1) < chance){
+            chance = get_chance(board, x+2, y+1);
+            *posX = x + 2;
+            *posY = y + 1;
+        }
+    }
+    
+    if(x+2<8  && y-1>=0 && board[x+2][y-1]==' '){
+        if(get_chance(board, x+2, y-1) < chance){
+            chance = get_chance(board, x+2, y-1);
+            *posX = x + 2;
+            *posY = y - 1;
+        }
+    }
+    
+    if(x-2>=0 && y+1<8  && board[x-2][y+1]==' '){
+        if(get_chance(board, x-2, y+1) < chance){
+            chance = get_chance(board, x-2, y+1);
+            *posX = x - 2;
+            *posY = y + 1;
+        }
+    }
+    
+    if(x-2>=0 && y-1>=0 && board[x-2][y-1]==' '){
+        if(get_chance(board, x-2, y-1) < chance){
+            chance = get_chance(board, x-2, y-1);
+            *posX = x - 2;
+            *posY = y - 1;
+        }
+    }
+    
+    if(y+2<8  && x+1<8  && board[x+1][y+2]==' '){
+        if(get_chance(board, x+1, y+2) < chance){
+            chance = get_chance(board, x+1, y+2);
+            *posY = y + 2;
+            *posX = x + 1;
+        }
+    }
+    
+    if(y+2<8  && x-1>=0 && board[x-1][y+2]==' '){
+        if(get_chance(board, x-1, y+2) < chance){
+            chance = get_chance(board, x-1, y+2);
+            *posY = y + 2;
+            *posX = x - 1;
+        }
+    }
+    
+    if(y-2>=0 && x+1<8  && board[x+1][y-2]==' '){
+        if(get_chance(board, x+1, y-2) < chance){
+            chance = get_chance(board, x+1, y-2);
+            *posY = y - 2;
+            *posX = x + 1;
+        }
+    }
+    
+    if(y-2>=0 && x-1>=0 && board[x-1][y-2]==' '){
+        if(get_chance(board, x-1, y-2) < chance){
+            chance = get_chance(board, x-1, y-2);
+            *posY = y - 2;
+            *posX = x - 1;
+        }
+    }
+    
+    
+}
+
+//tampilkan board
+void view_board(char **board){
+    printf("ษอออหอออหอออหอออหอออหอออหอออหอออป\n");
+    for(int i=0; i<8; i++){
+        printf("บ");
+        for(int j=0; j<8; j++)
+            printf(" %c บ", board[i][j]);
+        if (i<7) printf("\nฬอออฮอออฮอออฮอออฮอออฮอออฮอออฮอออน\n");
+    }
+    printf("\nศอออสอออสอออสอออสอออสอออสอออสอออผ\n");
+}
+
+//gerakkan kuda hingga 64 langkah
+void move_knight(char **board, int *posX, int *posY, char token){
+    system("cls");
+    printf("x: %d  y: %d\n", *posX, *posY);
+    board[*posX][*posY] = token;
+    next_step(board, posX, posY);
+    board[*posX][*posY] = '$';
+    
+    token++;
+    if(token=='9'+1) token = 'A';
+    else if(token=='Z'+1) token = 'a';
+    else if(token=='z'+1) token = '&';
+    view_board(board);
+    getch();
+    
+    if (token == '&'+1)
+		return;
+    else move_knight(board, posX, posY, token);
+}
+
+int main(){
+    char r;
+    char **board;
+    int i, j;
+    do{  
+        system("cls");
+        board = (char**)malloc(sizeof(char*)*8);
+        for(i=0; i<8; i++){
+            board[i] = (char*)malloc(sizeof(char) * 8);
+            for(j=0; j<8; j++) board[i][j] = ' ';
+        }
+        
+        scanf("%d%d", &i, &j); 
+        
+        board[i][j] = '$';
+        view_board(board);
+        getch();
+        
+        move_knight(board, &i, &j, '0');
+        
+        printf("\nIngin mengulang [Y/N]? ");
+        do{
+            r = getch();
+        }while(r!='y' && r!='Y' && r!='n' && r!='N');
+        for(i=7; i>=0; i--) free(board[i]);
+        free(board);
+    }while(r=='Y' || r=='y');
+}
---
+++ Knight Tour Solver/README.md
@@ -0,0 +1,4 @@
+# Knight's Tour Solver
+
+The purpose of this code is to solve a Knight's tour problem.
+Follow this link to read about Knight's Tour: http://en.wikipedia.org/wiki/Knight%27s_tour
