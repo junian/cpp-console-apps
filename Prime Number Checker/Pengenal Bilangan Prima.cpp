8b087434e62fa57e6ef887f76498ecd735d80b78
junian <junian.triajianto@gmail.com>
2013-08-29 09:37:22 +0700

    added Prime Number Checker

---
+++ Prime Number Checker/Pengenal Bilangan Prima.cpp
@@ -0,0 +1,39 @@
+//Pengenal Bilangan Prima
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <math.h>
+
+char toUpper(char);
+
+main(){
+  char r, isPrime;
+  int n;
+  do{
+    system("cls");
+    isPrime = 1;
+    printf("Pengenal Bilangan Prima\n");
+    printf("=======================\n\n");
+    printf("Input N : ");
+    scanf("%d", &n);
+    for(int i=2; i<=sqrt(n); i++){
+      if(n % i == 0)
+        isPrime = 0;
+    }
+    if(isPrime && n >= 2)
+      printf("\n%d adalah BILANGAN PRIMA", n);
+    else
+      printf("\n%d adalah BUKAN BILANGAN PRIMA", n);
+    printf("\n\nIngin Mengulang [Y/N]? ");
+    do{
+      r = getch();
+      r = toUpper(r);
+    }while(r != 'Y' && r != 'N');
+  }while(r == 'Y');
+}
+
+char toUpper(char x){
+  if (x >= 97 && x <= 122) x = x - 32;
+  return x;
+}
---
+++ Prime Number Checker/README.md
@@ -0,0 +1,4 @@
+Prime Number Checker
+====================
+
+The purpose of this code is to check whether a number is prime or not.
