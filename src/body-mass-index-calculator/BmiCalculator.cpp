89b7d46b9608fb9bc29689e858f20bad062ed3f3
Junian Triajianto <author@junian.dev>
2025-08-27 14:49:49 +0700

    Fix BMI

--- src/body-mass-index-calculator/BmiCalculator.cpp
+++ src/body-mass-index-calculator/BmiCalculator.cpp
@@ -11,44 +11,58 @@
 
 char toUpper(char x);
 
-int main(){
+int main()
+{
   char r;
-  do{
+  do
+  {
     system("clear");
-    float TB, IMT;
-    int BB;
-    printf("Body Mass Index Calculator\n");
-    printf("=========================\n\n");
-    printf("BB   : Berat Badan (kg)\n");
-    printf("TB   : Tinggi Badan (m)\n");
-    printf("IMT  : Indeks Massa Tubuh\n\n");
-    printf("BB   : ");
-    scanf("%d", &BB);
-    printf("TB   : ");
-    scanf("%f", &TB);
-    IMT = BB / (TB * TB);
-    printf("IMT  : %0.2f\n\n", IMT);
-    printf("Anda Termasuk "); 
-    if(IMT < 18.5)
-      printf("KURUS");
-    else if(IMT < 23)
-      printf("IDEAL");
-    else if(IMT < 25)
-      printf("AGAK GEMUK");
-    else if(IMT < 30)
-      printf("GEMUK");
+
+    float bodyHeightInM, bodyMassIndex;
+    int bodyWeightInKg;
+
+    printf("┌──────────────────────────────┐\n");
+    printf("│  Body Mass Index Calculator  │\n");
+    printf("├──────┼───────────────────────┤\n");
+    printf("│ BW   │ Body Weight (kg)      │\n");
+    printf("│ BH   │ Body Height (m)       │\n");
+    printf("│ BMI  │ Body Mass Index       │\n");
+    printf("└──────┴───────────────────────┘\n\n");
+
+    printf("BW   : ");
+    scanf("%d", &bodyWeightInKg);
+
+    printf("BH   : ");
+    scanf("%f", &bodyHeightInM);
+
+    bodyMassIndex = bodyWeightInKg / (bodyHeightInM * bodyHeightInM);
+
+    printf("BMI  : %0.2f\n\n", bodyMassIndex);
+
+    printf("Your result is ");
+    if (bodyMassIndex < 18.5f)
+      printf("Underweight (Below 18.5)");
+    else if (bodyMassIndex < 25.0f)
+      printf("Healthy (18.5 – 24.9)");
+    else if (bodyMassIndex < 30)
+      printf("Overweight (25.0 – 29.9)");
     else
-      printf("SANGAT GEMUK");
-    printf("\n\nIngin Mengulang [Y/N]? ");
-    do{
+      printf("Obesity (30.0 or above)");
+
+    printf("\n\nTry again? [Y/n]? ");
+
+    do
+    {
       r = getchar();
       r = toUpper(r);
-    }while(r != 'Y' && r != 'N');
-  }while(r == 'Y');
+    } while (r != 'Y' && r != 'N');
+  } while (r == 'Y');
   return 0;
 }
 
-char toUpper(char x){
-  if (x >= 97 && x <= 122) x=x-32;
+char toUpper(char x)
+{
+  if (x >= 'a' && x <= 'z')
+    x = x - 'a' + 'A';
   return x;
 }
--- src/body-mass-index-calculator/README.md
+++ src/body-mass-index-calculator/README.md
@@ -1,5 +1,4 @@
-Ideal Weight Calculator
-=======================
+# Body Mass Index Calculator
 
 This sample code will calculate if you have an ideal body weight.
 You just need to input your weight in kg and your height in meter.
@@ -8,3 +7,7 @@ The program will show output whether you're skinny (KURUS), IDEAL, or fat (GEMUK
 ```shell
 gcc -o app.exe BmiCalculator.cpp && ./app.exe
 ```
+
+## Reference
+
+https://www.nhlbi.nih.gov/calculate-your-bmi
