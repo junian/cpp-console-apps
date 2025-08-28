db9e16fd4e078b8da1d846346e61569b76b261bf
Junian Triajianto <author@junian.dev>
2025-08-28 13:06:39 +0700

    Update config

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
-  title: 'My Site',
+  base: '/cpp-awesome-pack/', // For hosting to https://example.com/cpp-awesome-pack/
+  title: 'C/C++ Console Apps',
   icon: '/rspress-icon.png',
   logo: {
     light: '/rspress-light-logo.png',
