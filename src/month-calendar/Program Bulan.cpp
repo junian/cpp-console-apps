ad4f989298a7cf6df984fd8e0e3838b6a39b4596
Junian Triajianto <author@junian.dev>
2025-08-28 09:42:48 +0700

    Refactor

--- src/month-calendar/Program Bulan.cpp
+++ src/month-calendar/Program Bulan.cpp
@@ -1,33 +1,66 @@
 #include <stdio.h>
 
+int main()
+{
+  while (1)
+  {
+    int month, year;
 
-int main(){
-  int bln, thn;
-  printf("Input Bulan [1-12]: ");
-  scanf("%d", &bln);
-  printf("Input Tahun: ");
-  scanf("%d", &thn);
-  printf("\n");
-  switch(bln){
-  case 1 : printf("Januari, 31"); break;
-  case 2 : printf("Februari, "); 
-           if((thn % 4 == 0 && thn % 100 != 0) || thn % 400 == 0)
-             printf("29");
-           else
-             printf("28");
-           break;
-  case 3 : printf("Maret, 31"); break;
-  case 4 : printf("April, 30"); break;
-  case 5 : printf("Mei, 31"); break;
-  case 6 : printf("Juni, 30"); break;
-  case 7 : printf("Juli, 31"); break;
-  case 8 : printf("Agustus, 31"); break;
-  case 9 : printf("September, 30"); break;
-  case 10: printf("Oktober, 31"); break;
-  case 11: printf("November, 30"); break;
-  case 12: printf("Desember, 31"); break;
+    printf("Enter month [1-12]: ");
+    fflush(stdout);
+    scanf("%d", &month);
+
+    printf("Enter year: ");
+    fflush(stdout);
+    scanf("%d", &year);
+
+    puts("");
+
+    switch (month)
+    {
+    case 1:
+      printf("January, 31");
+      break;
+    case 2:
+      printf("February, ");
+      if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        printf("29");
+      else
+        printf("28");
+      break;
+    case 3:
+      printf("March, 31");
+      break;
+    case 4:
+      printf("April, 30");
+      break;
+    case 5:
+      printf("May, 31");
+      break;
+    case 6:
+      printf("June, 30");
+      break;
+    case 7:
+      printf("July, 31");
+      break;
+    case 8:
+      printf("August, 31");
+      break;
+    case 9:
+      printf("September, 30");
+      break;
+    case 10:
+      printf("October, 31");
+      break;
+    case 11:
+      printf("November, 30");
+      break;
+    case 12:
+      printf("December, 31");
+      break;
+    }
+    printf(" days.");
+    puts("");
   }
-  printf(" hari");
-  getchar();
   return 0;
 }
